9b586b5f704817bddacb38b60ee29f9d7d4e34dc
Douglas Nicida <douglasnicida@gmail.com>
2024-06-16 21:56:03 -0300

    [feat] - dynamic values for ProjAndDetails

--- src/pages/ProjAndDetails.jsx
+++ src/pages/ProjAndDetails.jsx
@@ -1,16 +1,31 @@
 import { useParams } from "react-router-dom";
-import data from "../data/projetos.json"
 import "../styles/Projetos.css"
 import { ProgressBar, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ProjAndDetails() {
     const { id } = useParams()
-    const item = data.find(item => item.id === parseInt(id));
 
-    if (!item) {
-        return <div>Project not found</div>;
-    }
+    const [project, setProject] = useState(null);
+    const [subjects, setSubjects] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        try {
+            axios.get(`http://localhost:3000/projects/${id}`).then(result => {setProject(result.data.payload); setIsLoading(false);});
+        } catch(e) {
+            console.log(e)
+        }
+
+        try {
+            axios.get(`http://localhost:3000/projects/${id}/subjects`).then(result => setSubjects(result.data));
+            isLoading(false);
+        } catch(e) {
+            console.log(e);
+        }
+    }, [])
 
     return (
         <>
@@ -18,20 +33,20 @@ export default function ProjAndDetails() {
 
             <div className="proj-title">
                 <h1 style={{ fontWeight: "bold" }}>Lorem Ipsum Dolor Sit Amet</h1>
-                {item.isDonate ?
+                {project?.isDonate ?
                     <div className="proj-donation">
-                        <img src={item.img} alt="" style={{ height: "350px", width: "600px" }} />
+                        <img src={project?.img} alt="" style={{ height: "350px", width: "600px" }} />
                         <div className="proj-donation-descr">
                             <h2>Faça uma doação!</h2>
                             Lorem ipsum dolor sit amet. Aut iure aliquid eos quis distinctio est tempore nostrum ut sint
                             alias. Ut laborum blanditiis est perferendis neque ut alias laboriosam. Non dolorem saepe ut
                             harum omnis et esse doloribus aut dolorem dolor et aliquam tempora.
                             <div className="donation">
-                                <h4><strong>R${item.valorDoado.toFixed(2)} doados</strong></h4>
-                                <ProgressBar variant="success" now={item.valorDoado} max={item.valorEsperado} />
+                                <h4><strong>R${project?.valorDoado.toFixed(2)} doados</strong></h4>
+                                <ProgressBar variant="success" now={project?.valorDoado} max={project?.valorEsperado} />
                                 <button className="donation-button">
-                                    <Link to={`/doacao/${item.id}`}
-                                        style={{ textDecoration: "none", color: "black" }}>
+                                    <Link to={`/doacao/${project?.id}`}
+                                        style={{ textDecoration: "none", color: "black", fontSize: "14px", padding: "0 9px" }}>
                                         Doar
                                     </Link>
                                 </button>
@@ -39,30 +54,43 @@ export default function ProjAndDetails() {
                         </div>
                     </div>
                     :
-                    <img src={item.img} alt="" style={{ height: "350px", width: "600px" }} />
+                    <img src={project?.img} alt="" style={{ height: "350px", width: "600px" }} />
                 }
             </div>
+            
             <div className="hashtags">
-                {item.hashtags.map((hashtag, index) => 
-                    <div className="hashtag-item" key={index}><span>#{hashtag}</span></div>
+                {subjects?.payload.map((subject) => 
+                    <div className="hashtag-item" key={subject}><span>#{subject}</span></div>
                 )}
             </div>
             <div className="proj-text">
-                <strong>Descrição e objetivos</strong>
-                <br />Et consequuntur voluptates aut sunt placeat aut nihil animi hic neque nesciunt At suscipit sunt ut corporis omnis. Aut ullam quos et natus error et sunt numquam nam tempora porro eum porro dicta et quis quae ea voluptates soluta.
-                <br />
-                <br /><strong>Responsável(is)</strong>
-                <br />Et consequuntur voluptates.
-                <br />
-                <br /><strong>Vagas</strong>
-                <br />Qui corporis cupiditate et rerum.
-                <br />
-                <br /><strong>Período de atividades</strong>
-                <br />Lorem ipsum dolor sit amet.
-                <br />
-                <br /><strong>Carga horária</strong>
-                <br />Lorem ipsum
-                <br />
+                <div className="proj-text-item">
+                    <strong>Descrição e objetivos</strong>
+                    {project?.description}
+                </div>
+
+                <div className="proj-text-item">
+                    <strong>Responsável(is)</strong>
+                    Nome do supervisor
+                </div>
+                
+                <div className="proj-text-item">
+                    <strong>Vagas ({project?.slots})</strong>
+                    <ul>
+                        <li>Nome pessoa</li>
+                        <li>Vaga aberta</li>
+                    </ul>
+                </div>
+
+                <div className="proj-text-item">
+                    <strong>Período de atividades</strong>
+                    {project?.workload} horas
+                </div>
+
+                <div className="proj-text-item">
+                    <strong>Carga horária</strong>
+                    Lorem ipsum
+                </div>
             </div>
                 </Container>
         </>
