89890ebda144d3ce1b40c864c9bf9327e2763a42
Douglas Nicida <douglasnicida@gmail.com>
2024-06-20 22:40:00 -0300

    [feat] - skeleton load for project lists

--- src/pages/ProjDisponiveis.jsx
+++ src/pages/ProjDisponiveis.jsx
@@ -1,5 +1,5 @@
 import "../styles/Projetos.css"
-import { Container, Modal, Form, Button } from "react-bootstrap"
+import { Container } from "react-bootstrap"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { useAuth } from "../contexts/AuthContext"
@@ -12,8 +12,15 @@ export default function ProjDisponiveis() {
 
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        const tweets_skeleton = document.querySelector(".tweets-skeleton");
+        const tweet_skeleton = document.querySelector(".tweet-skeleton");
+        for (let i = 0; i < 5; i++) {
+            tweets_skeleton?.append(tweet_skeleton.cloneNode(true));
+        }
+
         const fetchProjects = async () => {
             try {
                 const response = await axios.get(`
@@ -28,6 +35,7 @@ export default function ProjDisponiveis() {
                 currentPage === 1 &&
                     setTotalPages(Array.from({ length: payload.totalPages }, (_v, i) => i + 1));
                 setProjects(payload.items);
+                setIsLoading(false);
             } catch (err) {
                 console.log(err);
             }
@@ -41,7 +49,28 @@ export default function ProjDisponiveis() {
             <Container>
                 <h1 className="title">Projetos Disponíveis</h1>
             </Container>
-            <Project projects={projects} />
+            {
+                (!isLoading) &&
+                <Project projects={projects}/>
+            }
+
+            {
+                (isLoading) && 
+                // <div id="content-container">Carregando...</div>
+                <div className="tweets-skeleton">
+                    <div className="tweet-skeleton">
+                        <div className="content-1">
+                            <div className="line"></div>
+                            <div className="line"></div>
+                            <div className="line"></div>
+                        </div>
+                        <div className="content-2">
+                            <div className="line"></div>
+                            <div className="line"></div>
+                        </div>
+                    </div>
+                </div>
+            }
             <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
         </>
     )
